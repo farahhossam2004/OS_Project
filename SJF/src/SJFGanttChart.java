b83d5af8b88f71a8f3ffd0b9bea6d7eee99ea3d0
Farah Hossam <farah.alden2004@gmail.com>
2024-05-04 00:10:57 +0300

    Updated Gantt Chart <3 Co-authored-by: Zeyad

---
+++ SJF/src/SJFGanttChart.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.*;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class SJFGanttChart extends Application{
+
+    static ArrayList<Process> servedProcesses = ProcessManagement.getServedProcesses();
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+        LineChart<Number, String> lineChart = createLineChart();
+
+        // Add tasks to the chart 
+        for(Process p : servedProcesses)
+        {
+            addProcess(lineChart, p.getID(), p.getStartClock(), p.getEndClock(), "red");
+        }
+        Scene scene = new Scene(new StackPane(lineChart), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private LineChart<Number, String> createLineChart()
+    {
+        CategoryAxis yAxis = new CategoryAxis();
+        NumberAxis xAxis = new NumberAxis();
+        xAxis.setAutoRanging(true);
+        xAxis.setUpperBound((int)ProcessManagement.CalculateTotalBurstTime()); // done
+        
+        LineChart<Number, String> lineChart = new LineChart<>(xAxis, yAxis);
+        lineChart.setTitle("Shortest Job First");
+        xAxis.setLabel("Time"); 
+        yAxis.setLabel("Processes");
+        lineChart.setLegendVisible(false);
+        return lineChart;
+    }
+
+    private void addProcess(LineChart<Number, String> lineChart, int ProcessID, double startTime , double endTime, String Color)
+    {
+        XYChart.Series<Number, String> series = new XYChart.Series<>();
+        series.getData().add(new XYChart.Data<>(startTime, String.valueOf(ProcessID)));
+        series.getData().add(new XYChart.Data<>(endTime, String.valueOf(ProcessID)));
+        series.setName("Process " + ProcessID);
+        lineChart.getData().add(series);
+        
+    }
+}
